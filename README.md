749acd86b26aab569aefdcc709e5d68bd494dd83
Walter Goins <wgoinz@gmail.com>
2019-01-25 11:58:13 -0500

    updated App component with colored jumbotron. updated readme file. included google fonts and bootstrap 4

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FightingGameTournaments
+
+Resources
+Google Fonts - https://fonts.google.com/featured/Headline-Worthy+Serifs?selection.family=Sanchez
+Request - https://www.npmjs.com/package/request
+Bootstrap 4 - https://getbootstrap.com/docs/4.0/getting-started/introduction/
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from "styled-components"
 import './App.css';
 
+const Main = styled.div`
+background-color:#5EC9DB;
+font-family: 'Sanchez', serif;
+h1{
+  font-size: 75px;
+}`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Main className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Fighting Game Tournaments</h1>
+        </div>
+      </Main>
     );
   }
 }
